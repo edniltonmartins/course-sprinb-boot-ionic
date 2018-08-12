b7d6a27e7991b59e79c380fac365328ff45bcf6f
edsilva_gft <edsilva.gft@sulamerica.com.br>
2018-08-12 03:28:11 -0300

    Init project

---
+++ src/main/java/com/br/coursespringbootionic/CourseSprinbBootIonicApplication.java
@@ -0,0 +1,12 @@
+package com.br.coursespringbootionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSprinbBootIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSprinbBootIonicApplication.class, args);
+	}
+}
