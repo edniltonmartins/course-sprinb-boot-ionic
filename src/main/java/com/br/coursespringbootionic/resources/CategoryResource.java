04bf99b6ca15a920737df1aadab06a1f6b2df8fb
ednilton17@gmail.com <ednilton17@gmail.com>
2018-08-12 04:11:39 -0300

    Testing the use of domain category

--- src/main/java/com/br/coursespringbootionic/resources/CategoryResource.java
+++ src/main/java/com/br/coursespringbootionic/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.br.coursespringbootionic.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.br.coursespringbootionic.domain.Category;
+
 @RestController
-@RequestMapping(value="categories")
+@RequestMapping(value = "categories")
 public class CategoryResource {
-	
+
 	@GetMapping
-	public String testInit() {
-		return "Ok";
+	public List<Category> list() {
+		Category category = new Category(1, "Test");
+		Category category2 = new Category(2, "Dev");
+		
+		List<Category> categories = new ArrayList<Category>();
+		
+		categories.add(category);
+		categories.add(category2);
+		
+		return categories;
 	}
 
 }
